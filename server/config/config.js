013a3deaba876eb619c9cf61dd0a586d149c8bfd
Angie <angiecata.30@gmail.com>
2020-08-20 17:03:05 -0500

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/eyedata';
 } else {
-    urlDB = process.env.NONGO_URI;
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
